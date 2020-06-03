27c782e06300120e1be525ef87250756d5f60e93
neutronstar-ss <neutron.star.system.services@gmail.com>
2020-06-03 07:03:54 -0300

    first commit

---
+++ src/test/java/br/com/confeccao/ConfeccaoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.confeccao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfeccaoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
